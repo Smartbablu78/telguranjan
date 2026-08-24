71ef36e11d2bdd36e4396635d01077538cd24474
Smart Bablu <smartbablu78@gmail.com>
2026-08-24 15:13:39 +0530

    telguranjan visual code

---
+++ script.js
@@ -0,0 +1,10 @@
+// ==========================================
+// BABLU BHAIYA OFFICIAL
+// ==========================================
+
+// इस page में कोई auto redirect नहीं है.
+// कोई countdown या progress bar भी नहीं है.
+
+// अगर बाद में कोई animation या extra
+// functionality चाहिए होगी,
+// तो इसी file में add की जा सकती है.
